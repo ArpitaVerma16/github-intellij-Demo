34220627acf4f32ad9593665a31590b66ee8d299
ArpitaVerma316 <a8729060@gmail.com>
2025-07-29 16:54:35 +0530

    added inheritance

---
+++ src/OOPS/childAndBaseClass.java
@@ -0,0 +1,31 @@
+package OOPS;
+
+public class childAndBaseClass {
+    static class Person{
+        public int age;
+        public String name;
+        public String address;
+    }
+    static class Student extends  Person{
+        public int RollNo;
+        public int marks;
+        Student(int age,String name,String address,int RollNo,int marks){
+            this.age = age;
+            this.name = name;
+            this.address = address;
+            this.RollNo = RollNo;
+            this.marks = marks;
+        }
+        public void display(){
+            System.out.println(name);
+            System.out.println(age);
+            System.out.println(address);
+            System.out.println(RollNo);
+            System.out.println(marks);
+        }
+    }
+    public static void main(String[] args) {
+     Student s1 = new Student(18,"Arpita","Gorakhpur",17,98);
+     s1.display();
+    }
+}
